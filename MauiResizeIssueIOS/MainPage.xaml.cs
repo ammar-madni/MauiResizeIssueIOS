8ac6467d535fbd04292614164272dd74f9a41c7a
Ammar <ammar.madni@yahoo.co.uk>
2022-07-18 21:31:38 +0100

    reproduce resizing issue on iOS

--- MauiResizeIssueIOS/MainPage.xaml.cs
+++ MauiResizeIssueIOS/MainPage.xaml.cs
@@ -9,17 +9,10 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void OnToggleClicked(object sender, EventArgs e)
+    {
+        AddressDetails.IsVisible = !AddressDetails.IsVisible;
+    }
 }
 
 
